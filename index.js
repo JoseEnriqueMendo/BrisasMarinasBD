efb09e6a3f2d33cd93710cc1de37fdfbd1253568
JoseEnriqueMendo <dinofiesta123@gmail.com>
2024-05-13 19:30:38 -0500

    add message to bd

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const cors = require("cors");
-const routes = require("./routes/index");
-const dotenv = require("dotenv");
+const cors = require('cors');
+const routes = require('./routes/index');
+const dotenv = require('dotenv');
+const { client } = require('./db/index');
+
 dotenv.config();
 
 //middleware
@@ -10,7 +12,18 @@ app.use(express.json()); //req.body
 app.use(cors());
 
 //ROUTES//
-app.use("/", routes);
+app.use('/', routes);
+app.get('/message', async (req, res) => {
+  try {
+    const queryResult = await client.query('SELECT $1::text as mensaje', [
+      'Hola, Haciendo Conexcion con la base de datos',
+    ]);
+    const mensaje = queryResult.rows[0].mensaje;
+    res.status(200).send(mensaje);
+  } catch (error) {
+    res.status(500).send('ERROR AL CONECTAR A LA BD');
+  }
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
